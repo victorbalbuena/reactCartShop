6d587ea5a3c33a3e50440a13d915a880b7a7db2f
Victor Balbuena <balbuenabellovictorantonio@gmail.com>
2023-06-29 16:48:27 -0600

    basic project settings

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetails from "./pages/ProductDetails";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <div className="overflow-hidden">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+        </Routes>
+      </Router>
+      <Sidebar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
